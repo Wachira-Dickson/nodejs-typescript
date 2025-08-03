f768750dc18b2ef73f576f6a475b7e8f4df7a1e0
Your Name <dicksonwachira886@gmail.com>
2025-08-03 20:04:45 +0300

    set up Express server with middleware and routes using TypeScript

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,46 @@
-console.log("Hello Node JS from Typescript.");
+import express, { Express, Request, Response, NextFunction } from "express";
 
-function getName(name: string){
-    return name;
+const app: Express = express();
+const port = 3000;
+
+app.use(express.json());
+
+interface CustomRequest extends Request {
+    startTime?: number;
 }
 
-console.log(getName("Dickson Wachira"));
+//middleware -> add startTime to request
+app.use((req: CustomRequest, res: Response, next: NextFunction) => {
+    req.startTime = Date.now();
+    next();
+})
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello. Typescript with express");
+});
+//post route -> new user -> name, email -> req.body
+// -> /user/:id?name -> Request <{}, {}, {}, {}>
+
+interface User {
+    name: string;
+    email: string;
+}
+
+app.post('/user',(req: Request<{}, {}, User>, res: Response) => {
+    const { name, email } = req.body;
+    res.json({
+        message: `User ${name} with email ${email} created successfully!`
+    });
+});
+
+//get user based on id
+app.get('/users/:id', (req: Request<{ id: string }>, res: Response) => {
+    const { id } = req.params;
+    res.json({
+        userId: id,
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
